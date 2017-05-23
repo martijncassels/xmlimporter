e912a3f46250bab43cf42f05075d41fa84c48596
Martijn Cassels <info@martijncassels.nl>
2017-05-23 23:22:40 +0200

    added xml support and promises

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('bluebird');
+var XML = Promise.promisifyAll(require('xml-simple'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	XML.parse("<p><b>test</b></p>")
+	.then(function(parsed) {
+		console.log(parsed)
+	})
+	.then(function(parsed){
+  		res.render('index', { title: 'Express' });
+	})
+	.catch(function(error)){
+		console.log(error);
+	});
 });
 
 module.exports = router;

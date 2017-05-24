73923cd361b9088355e30b40b3c50ccc54fe79a6
Martijn Cassels <info@martijncassels.nl>
2017-05-24 22:46:49 +0200

    24-05-2017 | xml from stream

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var xmlparser = require('express-xml-bodyparser');
+var multiparty = require('multiparty');
 
-var index = require('./routes/index');
+//var index = require('./routes/index');
 
 var app = express();
 
@@ -17,11 +19,61 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(xmlparser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
+app.post('/',function(req,res,next){
+	var count = 0;
+	var form = new multiparty.Form();
+	 
+	// Errors may be emitted 
+	// Note that if you are listening to 'part' events, the same error may be 
+	// emitted from the `form` and the `part`. 
+	form.on('error', function(err) {
+	  console.log('Error parsing form: ' + err.stack);
+	});
+	 
+	// Parts are emitted when parsing the form 
+	form.on('part', function(part) {
+	  // You *must* act on the part by reading it 
+	  // NOTE: if you want to ignore it, just call "part.resume()" 
+	 
+	  if (!part.filename) {
+	    // filename is not defined when this is a field and not a file 
+	    console.log('got field named ' + part.name);
+	    // ignore field's content 
+	    part.resume();
+	  }
+	 
+	  if (part.filename) {
+	    // filename is defined when this is a file 
+	    count++;
+	    console.log('got file named ' + part.name);
+	    // ignore file's content here 
+	    part.resume();
+	  }
+	 
+	  part.on('error', function(err) {
+	    // decide what to do 
+	    res.send('got an error! ',err);
+	  });
+
+	});
+ 
+// Close emitted after form parsed 
+	form.on('close', function() {
+	  console.log('Upload completed!');
+	  res.setHeader('text/plain');
+	  res.end('Received ' + count + ' files');
+	});
+	 
+	// Parse req 
+	form.parse(req);
+	res.send('done!');
+	});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var Promise = require('bluebird');
-var XML = Promise.promisifyAll(require('xml-simple'));
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var multer = require('multer'); // v1.3.0
+var upload = multer(); // for parsing multipart/form-data
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	XML.parse("<p><b>test</b></p>")
-	.then(function(parsed) {
-		console.log(parsed)
-	})
-	.then(function(parsed){
-  		res.render('index', { title: 'Express' });
-	})
-	.catch(function(error)){
-		console.log(error);
+	// XML.parseString("<p><b>test</b></p>")
+	// .then(function(parsed) {
+	// 	console.log(parsed)
+	// })
+	// .then(function(){
+ //  		res.render('index', { title: 'Express' });
+	// })
+	// .catch(function(error){
+	// 	console.log(error);
+	// });
+	var xml = "<order><orderrows><orderrow>1</orderrow><orderrow>2</orderrow></orderrows></order>";
+	parseString(xml,function(err,result){
+		if (err) {
+			next();
+		}
+		console.log(result);
+		var tmp = JSON.stringify(result, null, '-');
+		//res.render('index', { data : tmp });
+		res.type('application/json');
+   		res.send(tmp);
 	});
+
+});
+
+router.post('/',function(req, res, next) {
+	//console.log(req.body);
+	res.send(JSON.stringify(req.body));
+	//res.type('application/json');
+
+	// parser.parseString(req.body,function(err,result){
+	// 	if (err) {
+	// 		//next();
+	// 		console.log(err);
+	// 	}
+	// 	console.log('result: '+JSON.stringify(result));
+	// 	//var tmp = JSON.stringify(result, null, '-');
+		
+ //   		res.send('got response from: '+req.hostname+'<br>');
+	// });
+	//res.send('got response from: '+req.hostname+'\n');
+	//res.json(req.body);
 });
 
 module.exports = router;
